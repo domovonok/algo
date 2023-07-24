ef937112178e96409285d5088554b616a9e3111e
domovonok <74965408+domovonok@users.noreply.github.com>
2023-07-24 16:57:24 +0200

    Create z_function.cpp

---
+++ string/z_function.cpp
@@ -0,0 +1,20 @@
+template <typename T>
+vt<int> z_function(const T &s, int n) {
+    vt<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
+        z[i] = (r >= i ? min(z[i - l], r - i + 1) : 0);
+        while (z[i] + i < n && s[z[i]] == s[z[i] + i]) {
+            z[i]++;
+        }
+        if (r < z[i] + i - 1) {
+            l = i;
+            r = z[i] + i - 1;
+        }
+    }
+    return z;
+}
+ 
+template <typename T>
+vt<int> z_function(const T &s) {
+    return z_function(s, sz(s));
+}
