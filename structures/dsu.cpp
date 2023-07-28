67a68b716f84d796eabc5b07c1ea8d327318fd4c
domovonok <74965408+domovonok@users.noreply.github.com>
2023-07-28 22:29:05 +0200

    Update dsu.cpp

--- structures/dsu.cpp
+++ structures/dsu.cpp
@@ -3,18 +3,18 @@ struct dsu {
     int size;
     vt<int> p, s;
 
-    dsu(int n) {
+    void init(int n) {
         size = n;
         p.resize(size);
         iota(all(p), 0);
         s.assign(size, 1);
     }
 
-    inline int leader(int x) {
+    int leader(int x) {
         return (x == p[x] ? x : p[x] = leader(p[x]));
     }
 
-    inline void unite(int a, int b) {
+    void unite(int a, int b) {
         a = leader(a);
         b = leader(b);
         if (s[a] < s[b]) {
