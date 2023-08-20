51145261f11b1c30cafd7829bc9adf388c48323a
domovonok <74965408+domovonok@users.noreply.github.com>
2023-08-20 22:45:32 +0200

    Update template.cpp

--- misc/template.cpp
+++ misc/template.cpp
@@ -7,28 +7,6 @@ using namespace std;
 #define debug(x...)
 #endif
 
-using ll = long long;
-using ull = unsigned long long;
-using db = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using pdd = pair<db, db>;
-using tiii = tuple<int, int, int>;
-using str = string;
-
-#define vt vector
-#define pb push_back
-#define eb emplace_back
-#define ins insert
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define sz(x) (int)x.size()
-
-#define mp make_pair
-#define mt make_tuple
-#define fi first
-#define se second
-
 void solve() {
     
 }
@@ -36,8 +14,6 @@ void solve() {
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
     int tests = 1;
     // cin >> tests;
     while (tests--) {
