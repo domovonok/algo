15bed804df47392f52ccd9cf428f48607259f1e4
domovonok <74965408+domovonok@users.noreply.github.com>
2023-08-21 20:44:04 +0200

    Create SpaseTable.cpp

---
+++ structures/SpaseTable.cpp
@@ -0,0 +1,24 @@
+struct SparseTable {
+
+    int size;
+    vector<vector<int>> table;
+
+    SparseTable(const vector<int> &a) {
+        size = a.size();
+        int logn = __lg(size) + 1;
+        table.resize(logn);
+        table[0] = a;
+        for (int l = 1; l < logn; l++) {
+            table[l].resize(size - (1 << l) + 1);
+            for (int i = 0; i + (1 << l) <= size; i++) {
+                table[l][i] = min(table[l - 1][i], table[l - 1][i + (1 << (l - 1))]);
+            }
+        }
+    }
+
+    int rmq(int l, int r) {
+        int logn = __lg(r - l);
+        return min(table[logn][l], table[logn][r - (1 << logn)]);
+    }
+
+};
