7fe182214c818e9883488c5f34f0d553fef7d390
Arsenii <74965408+domovonok@users.noreply.github.com>
2023-10-17 14:19:59 +0300

    Update sparsetable.cpp

--- structures/sparsetable.cpp
+++ structures/sparsetable.cpp
@@ -5,10 +5,10 @@ struct sparsetable {
 
     sparsetable(const vector<int> &a) {
         size = a.size();
-        int logn = __lg(size) + 1;
-        table.resize(logn);
+        int logn = __lg(size);
+        table.resize(logn + 1);
         table[0] = a;
-        for (int l = 1; l < logn; l++) {
+        for (int l = 1; l <= logn; l++) {
             table[l].resize(size - (1 << l) + 1);
             for (int i = 0; i + (1 << l) <= size; i++) {
                 table[l][i] = min(table[l - 1][i], table[l - 1][i + (1 << (l - 1))]);
