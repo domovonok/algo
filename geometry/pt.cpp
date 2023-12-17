3f23c5393e6e436f59947d376f2a31f1dbf96dec
domovonok <74965408+domovonok@users.noreply.github.com>
2023-12-18 00:23:03 +0300

    Create pt.cpp

---
+++ geometry/pt.cpp
@@ -0,0 +1,47 @@
+struct pt {
+    ll x, y;
+
+    pt() {
+        x = 0;
+        y = 0;
+    }
+
+    pt(ll _x, ll _y) {
+        x = _x;
+        y = _y;
+    }
+
+    pt(pt a, pt b) {
+        x = b.x - a.x;
+        y = b.y - a.y;
+    }
+
+    pt operator+(pt other) {
+        return pt(x + other.x, y + other.y);
+    }
+
+    pt operator-(pt other) {
+        return pt(x - other.x, y - other.y);
+    }
+
+    ll operator*(pt other) {
+        return x * other.x + y * other.y;
+    }
+
+    ll operator%(pt other) {
+        return x * other.y - other.x * y;
+    }
+};
+
+istream& operator>>(istream& in, pt& p) {
+    in >> p.x >> p.y;
+    return in;
+}
+
+ll len2(pt p) {
+    return p.x * p.x + p.y * p.y;
+}
+
+ll dist2(pt a, pt b) {
+    return len2(pt(a, b));
+}
